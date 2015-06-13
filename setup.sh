ac6af6b67d260a1ebdeed866590efd6f60140251
Clayton Burlison <clburlison@gmail.com>
2015-06-12 23:26:15 -0500

    first commit

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export USERNAME="admin"
+export PASSWORD="letmein"
+adduser --system --shell /bin/false --group --disabled-password $USERNAME
+echo -ne "$PASSWORD\n$PASSWORD\n" | smbpasswd -a -s $USERNAME
+chown -R $USERNAME:$USERNAME /repo/
+chmod -R ugo+rwx /repo/
+chmod -R 777 /nbi/
